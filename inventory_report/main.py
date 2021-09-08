bedc3a64d6ef91a804a1273cf23d2ed5cbf3a30c
Sgtelles <sg.telles@gmail.com>
2021-09-07 23:02:00 -0300

    Alteração no main
    Criação do código na main.py

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,15 @@
-def main():
-    pass
+import sys
+import "reports/complete_report.py"
+import "reports/simple_report.py"
+
+def main(args):
+    if args[0] == "" and args[1] == "":
+        exit("Verfiquei os argumentos")
+
+    if args[1] == "completo":
+        CompleteReport.generate(args[0])
+    else:
+        SimpleReport.generate(args[0])
+
+if __name__ == "__main__":
+    main(sys.argv)
